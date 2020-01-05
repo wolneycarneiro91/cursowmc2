0381b097fab1f3ddd24792f6fc61283958515a18
wolneycarneiro91 <wolneycarneiro91@gmail.com>
2020-01-05 01:42:29 -0300

    primeira versao

---
+++ src/main/java/com/cteste/cursomc2/Cursmomc2Application.java
@@ -0,0 +1,13 @@
+package com.cteste.cursomc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursmomc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursmomc2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cteste/cursomc2/Cursmomc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cteste.cursomc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursmomc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
