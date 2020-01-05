a57b1caaa7b26ac975433dd579bad4ae68a88fa4
Wolney Carneiro <wolneycarneiro91@gmail.com>
2020-01-05 14:52:07 -0300

    Adicionando API categorias REST

--- src/main/java/com/cteste/cursomc2/resources/CategoriaResource.java
+++ src/main/java/com/cteste/cursomc2/resources/CategoriaResource.java
@@ -1,15 +1,25 @@
 package com.cteste.cursomc2.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cteste.cursomc2.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1,"informatica");
+		Categoria cat2 = new Categoria(2,"Escritorio");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 	
 	
