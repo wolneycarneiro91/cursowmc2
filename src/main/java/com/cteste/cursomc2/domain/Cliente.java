bbf1eba7887cfbb6c632e53fa91995b81087c419
Wolney Carneiro <wolneycarneiro91@gmail.com>
2020-02-02 17:55:34 -0300

    mudanca de JsonBackReference para JsonIgnore e adicao de metodo POST categoria

--- src/main/java/com/cteste/cursomc2/domain/Cliente.java
+++ src/main/java/com/cteste/cursomc2/domain/Cliente.java
@@ -15,8 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.cteste.cursomc2.domain.enums.TipoCliente;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 public class Cliente implements Serializable {
 
@@ -27,7 +26,7 @@ public class Cliente implements Serializable {
 	private String nome;
 	private String email;
 	private String cpfOuCnpj;
-	@JsonManagedReference
+	
 	@OneToMany(mappedBy="cliente")	
 	private List<Endereco> enderecos = new ArrayList<>();
 	private Integer tipo;
@@ -35,7 +34,7 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
